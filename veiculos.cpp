59366437327f8472225205218cda8e163835a831
EduardoHaagP <haagp001@gmail.com>
2025-10-29 12:41:44 -0300

    fix

---
+++ veiculos.cpp
@@ -0,0 +1,6 @@
+#include "veiculos.h"
+
+Veiculos::Veiculos()
+{
+
+}
---
+++ veiculos.h
@@ -0,0 +1,18 @@
+#ifndef VEICULOS_H
+#define VEICULOS_H
+#include <iostream>
+#include <string>
+
+
+
+class Veiculos
+{
+protected:
+    int ano;
+
+
+public:
+    Veiculos();
+};
+
+#endif // VEICULOS_H
